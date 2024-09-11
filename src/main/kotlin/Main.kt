7d9671a1087d2c7348ce6844374b0bb8762be6ad
Guilherme Ignácio <stormierearth9@gmail.com>
2024-09-11 17:15:17 -0300

    Questao 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -71,9 +71,17 @@ fun main() {
         soma += k
     }
 
-    println(soma)
+    println("SOMA = $soma")
 
     println("--- Fim Da Questão 3 ---")
 
+    println("a) 1, 3, 5, 7, 9\n" +
+            "b) 2, 4, 8, 16, 32, 64, 128\n" +
+            "c) 0, 1, 4, 9, 16, 25, 36, 49\n" +
+            "d) 4, 16, 36, 64, 100\n" +
+            "e) 1, 1, 2, 3, 5, 8, 13\n" +
+            "f) 2,10, 12, 16, 17, 18, 19, 20")
+
+    println("--- Fim Da Questão 4 ---")
 
 }
