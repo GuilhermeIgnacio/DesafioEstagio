c63646bf95524cd1ec331ce82afded0e408f7f6b
Guilherme Ignácio <stormierearth9@gmail.com>
2024-09-11 16:43:58 -0300

    Questao 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,39 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun fibonacciSequence(limit: Int, n: Int) {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var n1 = 0
+    var n2 = 1
+    var n3: Int
+
+    val fibonacciList = mutableListOf(0,1)
+
+    print("$n1 ")
+    print("$n2 ")
+
+    for (i in 3 .. limit) {
+        n3 = n1 + n2
+        fibonacciList.add(n3)
+        print("$n3 ")
+        n1 = n2
+        n2 = n3
+    }
+
+    if (n in fibonacciList) {
+        println("\nO número $n pertence a sequencia Fibonacci")
+    } else {
+        println("\nO número $n não pertence a sequencia Fibonacci")
+    }
+
+}
+
+fun main() {
+    print("Informe um limite: ")
+    val limit = readlnOrNull()?.toIntOrNull()
+
+    print("Digite um número para saber se pertence ou não a sequência Fibonacci ")
+    val n = readlnOrNull()?.toIntOrNull()
+
+    if (limit != null && n != null) {
+        fibonacciSequence(limit, n)
+    }
+
 }
